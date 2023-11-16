fc840042f7ba0ecc901f4b12b769d99a773c92dc
Forhad5194 <forhadhossain5194@gmail.com>
2023-11-16 09:20:40 +0600

    Menu part is Done

--- src/MenuCategory/MenuCategory.jsx
+++ src/MenuCategory/MenuCategory.jsx
@@ -1,15 +1,16 @@
 import MenuItems from "../Shared/MenuItems/MenuItems";
 
 
-const MenuCategory = ({items}) => {
-    return ( 
+const MenuCategory = ({ items }) => {
+    return (
         <div>
-            {/* { title && <Cover  image={menuImg} title={'OUR MENU'} subTile={'Would you like tye a dIsh'}   />} */}
-            <div className='grid md:grid-cols-2 gap-5'>
+
+            <div className='grid md:grid-cols-2 gap-5 mt-5 mb-10'>
                 {
                     items.map(item => <MenuItems key={item._id} item={item}></MenuItems>)
                 }
 
+                <button className="btn btn-outline items-center mt-8" >View Full  Menu</button>
             </div>
         </div>
     );
--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -8,6 +8,9 @@ import useMenu from "../../../Hook/useMenu";
 import MenuCategory from "../../../MenuCategory/MenuCategory";
 import SectionTitle from "../../../Shared/SectionTitle/SectionTitle";
 import img1 from "../../../assets/menu/dessert-bg.jpeg"
+import img2 from "../../../assets/menu/pizza-bg.jpg"
+import img3 from "../../../assets/menu/salad-bg.jpg"
+import img4 from "../../../assets/menu/soup-bg.jpg"
 
 
 const Menu = () => {
@@ -15,6 +18,9 @@ const Menu = () => {
     const [menu] = useMenu();
     const offered = menu.filter(item => item.category === 'offered');
     const dessert = menu.filter(item => item.category === 'dessert');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const salad = menu.filter(item => item.category === 'salad');
+    const soup = menu.filter(item => item.category === 'soup');
 
 
 
@@ -25,22 +31,37 @@ const Menu = () => {
 
     return (
         <div>
-            
+
             <Helmet>
                 <title>Menu</title>
             </Helmet>
-            <Cover  image={menuImg} title={'OUR MENU'} subTile={'Would you like tye a dIsh'}   />
-            
+            <Cover image={menuImg} title={'OUR MENU'} subTile={'Would you like tye a dIsh'} />
+
             <SectionTitle subHeading={"---Don't miss---"} heading={"TODAY'S OFFER"}>
 
             </SectionTitle>
-            
+
             <MenuCategory items={offered}></MenuCategory>
 
-            
-            <MenuCategory items={dessert} title={dessert} image={img1} >
-                
-            </MenuCategory>
+            <div >
+                <Cover image={img1} title={'Dessert'} subTile={'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} />
+                <MenuCategory items={dessert} >
+
+                </MenuCategory>
+            </div>
+            <div>
+                <Cover image={img2} title={'Pizza'} subTile={'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} />
+
+                <MenuCategory items={pizza} />
+            </div>
+            <div>
+                <Cover image={img3} title={'Salads'} subTile={'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} />
+                <MenuCategory items={salad} />
+            </div>
+            <div>
+                <Cover image={img4} title={'soups'} subTile={'Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'} />
+                <MenuCategory items={soup} />
+            </div>
         </div>
     );
 };
